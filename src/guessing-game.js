b4e9032a4c62b4669b72ee5e23ca63218b1f3658
abramsa1 <abramsa1@rambler.ru>
2019-10-04 01:36:24 +0700

    feat: complete Guessing-game task

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -2,18 +2,20 @@ class GuessingGame {
     constructor() {}
 
     setRange(min, max) {
-
+        this.startNumber = min;
+        this.limitNumber = max;
     }
 
     guess() {
-
+        return Math.ceil((this.startNumber + this.limitNumber) / 2);
     }
 
     lower() {
-
+        this.setRange(this.startNumber, Math.ceil((this.startNumber + this.limitNumber) / 2));
     }
 
     greater() {
+        this.setRange(Math.ceil((this.startNumber + this.limitNumber) / 2), this.limitNumber);
     }
 }
 
